575ce3054326a281cd78c059c0c55f5b7af825c7
rahul122703 <killbusyness@gmail.com>
2025-01-06 03:22:28 +0530

    solved some small bug

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// import Final from "../../react-projects-minimal/src/23-quiz/final/src/App";
-// import { AppProvider } from "../../react-projects-minimal/src/23-quiz/final/src/context";
-
-import Setup from "../../react-projects-minimal/src/23-quiz/setup/src/App";
-import { AppProvider } from "../../react-projects-minimal/src/23-quiz/setup/src/context";
+import App from "./App";
+import { AppProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      {/* <Final /> */}
-      <Setup />
+      <App />
     </AppProvider>
   </React.StrictMode>
 );
